cc974a4fd2124acc54d9c36d4fe12121f9c76f9c
Nicole Torres <torrestapia.n@gmail.com>
2022-09-23 13:34:47 -0300

    final tictactoe

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,5 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './component/App';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
-
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.getElementById("app"));
